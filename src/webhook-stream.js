9d5e924022205a4b9ae8e22ebafa190e735773a7
CapiGe <45999091+CapiGe@users.noreply.github.com>
2023-02-15 20:48:27 +0100

    Update webhook-stream.js

--- src/webhook-stream.js
+++ src/webhook-stream.js
@@ -5,15 +5,13 @@ module.exports = async client => {
     console.log("Starting webhook-stream.js ..")
     client.on('messageCreate', async message => {
         if (message.channel.id == process.env.INPUT_CHANNEL) {
-            console.log(message)
             let txt = message.content.toLowerCase();
             var villageOutput = guild.channels.cache.find(x => x.id === process.env.OUTPUT_VILLAGE)
             var allianceOutput = guild.channels.cache.find(x => x.id === process.env.OUTPUT_ALLIANCE)
             var village = message.author.tag.slice(0, -5)
             var msg = message.content.slice(12 + village.length)
             if (txt.startsWith('<village>')) {
-                if (process.env.USE_EMBEDS == 'true') {
-                    
+                if (process.env.USE_EMBEDS == 'true') {                    
                     let messageEmbed = new EmbedBuilder()
                         .setColor('#00cc66')
                         .addFields(
