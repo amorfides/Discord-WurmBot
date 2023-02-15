30428f61960241e8f77fef0ca71ebadf316241ce
CapiGe <45999091+CapiGe@users.noreply.github.com>
2023-02-15 19:36:18 +0100

    Update error.js

--- src/error.js
+++ src/error.js
@@ -1,9 +1,7 @@
-const { logTime } = require( '../functions.js');
-
 module.exports = {
 	name: 'error',
 	once: true,
 	execute(client, error) {
-			console.log(`${logTime()} An error event was sent by Discord.js: \n${JSON.stringify(error)}`, "error");
+			console.log(`An error event was sent by Discord.js: \n${JSON.stringify(error)}`, "error");
 		  },	
 };
