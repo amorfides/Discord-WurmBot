65a3233987c195cc0037ad5e97d19508eba6f9b5
Arne Libbrecht <arne.libbrecht@icloud.com>
2023-06-29 19:24:30 +0200

    add docker container, change working with id to working with names, set up watch for QOL while developing, put standard config for traefik

--- server/src/webhook-stream.js
+++ server/src/webhook-stream.js
@@ -1,14 +1,16 @@
 module.exports = async client => {
     const { EmbedBuilder } = require('discord.js')
     require('dotenv').config();
-    const guild = await client.guilds.cache.get(process.env.GUILD_ID)
+    const guild = await client.guilds.cache.find(x=>x.name===process.env.GUILD_NAME);
     console.log("Starting webhook-stream.js ..")
     client.on('messageCreate', async message => {
-        if (message.channel.id == process.env.INPUT_CHANNEL) {
+        if (message.channel.name == process.env.INPUT_CHANNEL) {
             let txt = message.content.toLowerCase();
-            var villageOutput = guild.channels.cache.find(x => x.id === process.env.OUTPUT_VILLAGE)
-            var allianceOutput = guild.channels.cache.find(x => x.id === process.env.OUTPUT_ALLIANCE)
-            var village = message.author.tag.slice(0, -5)
+            console.log(txt);
+            console.log(message.channel.name);
+            var villageOutput = guild.channels.cache.find(x => x.name === process.env.OUTPUT_VILLAGE)
+            var allianceOutput = guild.channels.cache.find(x => x.name === process.env.OUTPUT_ALLIANCE)
+            var village = process.env.VILLAGE_NAME;
             var msg = message.content.slice(12 + village.length)
             if (txt.startsWith('<village>')) {
                 if (process.env.USE_EMBEDS == 'true') {                    
@@ -36,7 +38,7 @@ module.exports = async client => {
                             });
                     allianceOutput.send({ embeds: [messageEmbed] });
                 } else {
-                    allianceOutput.send({ content: msg })
+                    allianceOutput.send({ content: txt.slice(12) });
                 }
             }
         }
